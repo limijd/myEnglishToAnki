11063efcf4926b88716f84d53c40241ec0053a15
Wei Li <limijd@protonmail.com>
2020-12-27 23:12:34 -0800

    englishToAnki

--- englishToAnki.py
+++ englishToAnki.py
@@ -12,79 +12,8 @@ import html2text
 SCRIPT_PATH=os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, "%s/evernote/lib"%SCRIPT_PATH)
 
-from EnChDict import EnChDict
 from EvernoteReader import EvernoteReader
-
-ecdict_db = "%s/myDictDB/ecdict.sqlite.db" % SCRIPT_PATH
-stardict_db = "%s/myDictDB/stardict.sqlite.db" % SCRIPT_PATH
-ANKI_ENGLISH_NOTE_FIELDS = ["word", "phonetic", "definition", "translation", "exchange", "detail", "bnc", "frq", "tts"]
-
-
-class EnglishToAnki:
-    def __init__(self, args):
-        self.en_ch_dict = EnChDict(stardict_db)
-        if args.enable_google_tts:
-            self.en_ch_dict.EnableGoogleTTS()
-        self.all_words = {}
-        self.import_words = {}
-        self.args = args
-        return
-
-    def processWordList(self, words):
-        assert isinstance(words, list)
-        for word in words:
-            word = word.strip()
-            self.all_words[word] = None
-            for p in word.split(","):
-                p = p.strip()
-                self.all_words[p] = None
-                for w in  re.findall(r'[a-zA-Z]+', p):
-                    if len(w) > 1:
-                        self.all_words[w] = None
-        return
-
-    def processTextList(self, textList):
-        for text in textList:
-            for w in re.findall(r'[a-zA-Z]+', text):
-                if len(w) > 1:
-                    self.all_words[w] = None
-        return
-
-    def lookup_startdict(self):
-        words = self.all_words.keys()
-        result, failed_list = self.en_ch_dict.lookup_stardict_sql(words)
-
-        for k in result:
-            self.all_words[k] = result[k]
-            self.import_words[k] = result[k]
-
-
-    def writeAnkiImportFile(self):
-        fp = open(self.args.out, "w")
-        anki_cards = self.en_ch_dict.result_to_anki(self.import_words, ANKI_ENGLISH_NOTE_FIELDS, self.getAnkiTTSDir())
-        for ac in anki_cards:
-            fp.write(ac.encode("utf-8"))
-            fp.write("\n")
-        fp.close()
-
-    def getAnkiDB(self):
-        fp = open(os.path.expanduser("~/.configs.secure/evernote_to_anki.config.json"), "r")
-        js = json.load(fp)
-        self.anki_db = js["anki_db"]
-        fp.close()
-        return self.anki_db
-
-    def getAnkiTTSDir(self):
-        fp = open(os.path.expanduser("~/.configs.secure/evernote_to_anki.config.json"), "r")
-        js = json.load(fp)
-        anki_db = js["anki_db"]
-        fp.close()
-
-        dirs = anki_db.split("/")
-        dirs[-1] = "collection.media"
-        self.anki_tts_dir  = "/".join(dirs)
-        return self.anki_tts_dir
-
+from EnToAnki import EnglishToAnki
 
 def main():
     """ entry of program """
